1923b9a49be7ce507c60a1930d521a25d6a6fcaa
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-01-31 16:27:58 +0600

    div added for selected elements

--- src/components/Selected.js
+++ src/components/Selected.js
@@ -2,17 +2,13 @@ const Selected = ({ selected }) => {
     return (
         <>
             {selected && selected.length > 0 && (
-                <select
-                    className="selected-items"
-                    multiple={true}
-                    defaultValue={selected}
-                >
-                    {selected.map((item, i) => (
-                        <option key={i} disabled>
-                            {item}
-                        </option>
-                    ))}
-                </select>
+                <div className="selected-items">
+                    <ol type="1">
+                        {selected.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                    </ol>
+                </div>
             )}
         </>
     );
